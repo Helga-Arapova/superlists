3db4dd8b359a60ca17f39a73ac0514a945e84cca
Helga.Arapova <helga.arapova@gmail.com>
2024-08-21 09:53:57 +0500

    Basic view no returns normal html

--- superlists/urls.py
+++ superlists/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-import lists
+from lists import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',lists.views.home_page)
+    path('', views.home_page)
+
+
 ]
