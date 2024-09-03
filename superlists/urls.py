4f3683f92c68c34adffe8dcb73cf6ffaacfbc9ca
Helga.Arapova <helga.arapova@gmail.com>
2024-09-03 10:55:46 +0500

    new URL, view and template to display lists

--- superlists/urls.py
+++ superlists/urls.py
@@ -19,7 +19,7 @@ from django.urls import path
 from lists import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('lists/the-only-list-in-the-world/', views.view_list, name='view_list'),
     path('', views.home_page)
 
 
