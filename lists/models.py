31bf5728da5ad755ca99b00ff116c0120dbb664d
Helga.Arapova <helga.arapova@gmail.com>
2024-09-06 10:44:40 +0500

    added a list field

--- lists/models.py
+++ lists/models.py
@@ -1,4 +1,13 @@
 from django.db import models
 
+
+class List(models.Model):
+    pass
+
+
 class Item(models.Model):
-    text=models.TextField(default='')
+    text = models.TextField(default='')
+    list = models.ForeignKey(List, default='', on_delete=models.SET_DEFAULT)
+
+
+
